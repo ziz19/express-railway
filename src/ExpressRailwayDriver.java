f587371e467ad05c35fb4bfaa518b2935b76aeda
Zinan Zhuang <ziz19@pitt.edu>
2019-04-12 23:22:37 -0400

    complete interactive console

--- src/ExpressRailwayDriver.java
+++ src/ExpressRailwayDriver.java
@@ -12,7 +12,7 @@ public class ExpressRailwayDriver {
     private String query;  //this will hold the query we are using
     private String legend;  //this will hold the table name of result
     private String dataFolder; //this will be the folder location to store data
-    private Scanner input;
+    private static Scanner input;
 
     public ExpressRailwayDriver() {
         final String currentDir = System.getProperty("user.dir");
@@ -207,6 +207,7 @@ public class ExpressRailwayDriver {
         		if(++counter == 10) {
                     System.out.println("\nEnter q to quit, anything to show more:");
                     showMoreResults = !"q".equals(input.nextLine());
+                    if(showMoreResults) System.out.println(legend);
                     counter = 0;
                 }
         	    }
@@ -306,6 +307,7 @@ public class ExpressRailwayDriver {
         		if(++counter == 10) {
                     System.out.println("\nEnter q to quit, anything to show more:");
                     showMoreResults = !"q".equals(input.nextLine());
+                    if(showMoreResults) System.out.println(legend);
                     counter = 0;
                 }
         	    }
@@ -710,7 +712,6 @@ public class ExpressRailwayDriver {
     }
 
 
-
     public static void main(String args[]) throws SQLException {
         String username, password;
         username = "postgres"; //This is your username in the PostgreSQL server
@@ -744,10 +745,27 @@ public class ExpressRailwayDriver {
 
             /* main run of the client app */
             boolean isRunning = true;
+            int mode = 0;
             while(isRunning) {
-                System.out.println("")
+                switch(mode) {
+                    case 0:
+                        mode = displayLoginScreen();
+                        break;
+                    case 1:
+                        mode = displayAdminScreen(db);
+                        break;
+                    case 2:
+                        mode = displayServiceScreen(db);
+                        break;
+                    case 3:
+                        isRunning = false;
+                        break;
+                    case 4:
+                        mode = displayThankList();
+                    default:
+                        mode = 0;
+                }
             }
-            System.out.println("Progrm exits.");
     	}
     	catch(Exception Ex)  {
     	    System.out.println("Error connecting to database.  Machine Error: " +
@@ -760,6 +778,277 @@ public class ExpressRailwayDriver {
     		 * Is very expensive to open a connection therefore you should not close it after every operation on database
     		 */
     		connection.close();
+            System.out.println("Connection closed.");
     	}
+    }
+
+    public static int displayLoginScreen() {
+        System.out.println("\nWelcome to Express Railway Database Terminal");
+        System.out.println("This is the Login Screen. Please enter:");
+        System.out.println("  1 -- if you are a data administrator");
+        System.out.println("  2 -- if you need passenger services");
+        System.out.println("  3 -- to exit the program");
+        System.out.println("  4 -- THANK LIST\n");
+        return Integer.parseInt(input.nextLine());
+    }
+
+    public static int displayAdminScreen(ExpressRailwayDriver db) {
+        System.out.println("\nDear Data Admin, you can enter:");
+        System.out.println("  1 -- to import the database");
+        System.out.println("  2 -- to export the database");
+        System.out.println("  3 -- to erase the database");
+        System.out.println("  4 -- go back to login screen\n");
+
+        int operation = Integer.parseInt(input.nextLine());
+        switch(operation) {
+            case 1:
+                System.out.println("\nCaution! This will overwrite the data in the database.");
+                System.out.println("Enter yes or y to continue, anything else to quit:");
+                String confirm = input.nextLine();
+                if("yes".equals(confirm) || "y".equals(confirm)) {
+                    System.out.printf("Importing Data.......................");
+                    db.importDatabase();
+                    System.out.println("Done");
+                }
+                break;
+            case 2:
+                System.out.println("\nCaution! This will overwrite the data in your data folder.");
+                System.out.println("Enter yes or y to continue, anything else to quit:");
+                confirm = input.nextLine();
+                if("yes".equals(confirm) || "y".equals(confirm)) {
+                    System.out.printf("Exporting Data.......................");
+                    db.exportDatabase();
+                    System.out.println("Done");
+                }
+                break;
+            case 3:
+                System.out.println("\nCaution! This will remove all data in the database.");
+                System.out.println("Enter yes or y to continue, anything else to quit:");
+                confirm = input.nextLine();
+                if("yes".equals(confirm) || "y".equals(confirm)) {
+                    System.out.printf("Deleting Data.......................");
+                    db.eraseDatabase();
+                    System.out.println("Done");
+                }
+                break;
+            case 4:
+                return 0;
+        }
+        return 1;
+    }
+
+    public static int displayServiceScreen(ExpressRailwayDriver db) {
+        System.out.println("\nPassenger Services are listed below");
+        System.out.println("Please select your operation by entering:\n");
+        System.out.println("Customer Services  ------------------------------");
+        System.out.println("  1. Add a passenger");
+        System.out.println("  2. Edit an existing passenger");
+        System.out.println("  3. View an existing passenger info\n");
+        System.out.println("Trip Services  ----------------------------------");
+        System.out.println("  4. Find a single route between two stations");
+        System.out.println("  5. Find a route combo between two stations");
+        System.out.println("  6. Reserve a route for a passenger\n");
+        System.out.println("Advanced Searches -------------------------------");
+        System.out.println("  7. Find trains that pass a station at day & time");
+        System.out.println("  8. Find routes that travel multiple railines");
+        System.out.println("  9. Find similar routes");
+        System.out.println("  10. Find stations which all trains pass");
+        System.out.println("  11. Find trains that do not stop at a station");
+        System.out.println("  12. Find routes that stop at x% stations or more");
+        System.out.println("  13. Display the schedule of a route");
+        System.out.println("  14. Find seats of a route at day & time\n");
+        System.out.println("Others  -----------------------------------------");
+        System.out.println("  15. Go back to login screen\n");
+
+        int operation = Integer.parseInt(input.nextLine());
+        switch(operation) {
+            case 1:
+                System.out.println("What's the first name?");
+                String fname = input.nextLine();
+                System.out.println("What's the last name?");
+                String lname = input.nextLine();
+                System.out.println("What's the street address?");
+                String street = input.nextLine();
+                System.out.println("What's the town address?");
+                String town = input.nextLine();
+                System.out.println("What's the postal address?");
+                String postal = input.nextLine();
+                System.out.println("\nKeep your ID safe.");
+                db.addCustomer(fname, lname, street, town, postal);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 2:
+                System.out.println("What's the passenger ID?");
+                int cid = Integer.parseInt(input.nextLine());
+                System.out.println("What's the first name?");
+                fname = input.nextLine();
+                System.out.println("What's the last name?");
+                lname = input.nextLine();
+                System.out.println("What's the street address?");
+                street = input.nextLine();
+                System.out.println("What's the town address?");
+                town = input.nextLine();
+                System.out.println("What's the postal address?");
+                postal = input.nextLine();
+                System.out.println("\nWe are editing your data......");
+                db.editCustomer(cid, fname, lname, street, town, postal);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 3:
+                System.out.println("What's the passenger ID?");
+                cid = Integer.parseInt(input.nextLine());
+                System.out.println("\nWe are retrieving your data......");
+                db.viewCustomer(cid);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 4:
+                System.out.println("What's the start station ID?");
+                int origin = Integer.parseInt(input.nextLine());
+                System.out.println("What's the destination station ID?");
+                int destination = Integer.parseInt(input.nextLine());
+                System.out.println("What is the day you want to travel?");
+                System.out.println("Enter Monday, Tuesday, Wednesday, etc.");
+                String day = input.nextLine();
+                System.out.println("Do you want to sort by:");
+                System.out.println("  1. nothing");
+                System.out.println("  2. fewest stops");
+                System.out.println("  3. most stations");
+                System.out.println("  4. least distance");
+                System.out.println("  5. most distance");
+                System.out.println("  6. lowest price");
+                System.out.println("  7. highest price");
+                System.out.println("  8. least time");
+                System.out.println("  9. most time");
+                int sortBy = Integer.parseInt(input.nextLine());
+                System.out.println("\nWe are find your routes......");
+                db.searchSingleRoute(origin, destination, day, sortBy);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 5:
+                System.out.println("What's the start station ID?");
+                origin = Integer.parseInt(input.nextLine());
+                System.out.println("What's the destination station ID?");
+                destination = Integer.parseInt(input.nextLine());
+                System.out.println("What is the day you want to travel?");
+                System.out.println("Enter Monday, Tuesday, Wednesday, etc.");
+                day = input.nextLine();
+                System.out.println("Do you want to sort by:");
+                System.out.println("  1. nothing");
+                System.out.println("  2. fewest stops");
+                System.out.println("  3. most stations");
+                System.out.println("  4. least distance");
+                System.out.println("  5. most distance");
+                System.out.println("  6. lowest price");
+                System.out.println("  7. highest price");
+                System.out.println("  8. least time");
+                System.out.println("  9. most time");
+                sortBy = Integer.parseInt(input.nextLine());
+                System.out.println("\nWe are find your routes......");
+                db.searchRouteCombo(origin, destination, day, sortBy);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 6:
+                System.out.println("What's the passenger ID?");
+                cid = Integer.parseInt(input.nextLine());
+                System.out.println("What's the route ID?");
+                int routeID = Integer.parseInt(input.nextLine());
+                System.out.println("What is the day you want to travel?");
+                System.out.println("Enter Monday, Tuesday, Wednesday, etc.");
+                day = input.nextLine();
+                System.out.println("\nNote! If there are more than one available options, a random one is reserved");
+                System.out.println("Reserved Seat: ");
+                db.reserveRoute(cid, routeID, day);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 7:
+                System.out.println("What's the station ID?");
+                int stationID = Integer.parseInt(input.nextLine());
+                System.out.println("What is the day you want to travel?");
+                System.out.println("Enter Monday, Tuesday, Wednesday, etc.");
+                day = input.nextLine();
+                System.out.println("What is the time you want to travel?");
+                System.out.println("For example, 02:28, 03:30, 05:40, etc.");
+                String time = input.nextLine();
+                System.out.println("Finding trains that pass the station at this time......");
+                db.findTrains4Station(stationID, day, time);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 8:
+                System.out.println("Finding cross railines routes ......");
+                db.findMultiRailRoutes();
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 9:
+                System.out.println("Note: similar routes go through same stations, but stop differently");
+                System.out.println("Finding similar routes.......This may take a while");
+                db.findSimilarRoutes();
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 10:
+                System.out.println("Finding stations that all trains pass......");
+                db.findStationsAllTrainsPass();
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 11:
+                System.out.println("What's the station ID?");
+                stationID = Integer.parseInt(input.nextLine());
+                System.out.println("Finding trains that do not pass this station.....");
+                db.findTrainsNotPass(stationID);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 12:
+                System.out.println("What's least number of percent for a route to stop?");
+                int threshold = Integer.parseInt(input.nextLine());
+                System.out.println("Finding routes that stops more or equal than that......");
+                db.findRoutesPercent(threshold);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 13:
+                System.out.println("What's the route ID?");
+                routeID = Integer.parseInt(input.nextLine());
+                System.out.println("Displaying all schedules......");
+                db.displayRouteSchedules(routeID);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 14:
+                System.out.println("What's the route ID?");
+                routeID = Integer.parseInt(input.nextLine());
+                System.out.println("What is the day you want to travel?");
+                System.out.println("Enter Monday, Tuesday, Wednesday, etc.");
+                day = input.nextLine();
+                System.out.println("What is the time you want to travel?");
+                System.out.println("For example, 02:28, 03:30, 05:40, etc.");
+                time = input.nextLine();
+                System.out.println("Finding remaining seats......");
+                db.findAvailableSeats(routeID, day, time);
+                System.out.println("Press enter to go back");
+                input.nextLine();
+                break;
+            case 15:
+                return 0;
+        }
+        return 2;
+    }
+
+    public static int displayThankList() {
+        System.out.println("\nTech Lead:    Zinan Zhuang\uD83D\uDE06");
+        System.out.println("Database Designer:    Zinan Zhuang\uD83D\uDE16");
+        System.out.println("Database Programmer:    Zinan Zhuang\uD83D\uDE15");
+        System.out.println("Application Programmer:    Zinan Zhuang\uD83E\uDD2F");
+        System.out.println("Technical Advisor:    Constantinos Costa\uD83D\uDE0E, Xiaozhong Zhang\uD83E\uDD14");
+        return 0;
     }
 }
